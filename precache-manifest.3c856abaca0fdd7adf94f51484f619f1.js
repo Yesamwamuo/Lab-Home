f01725bb69bfbb65a6135d02489dd38c4f82533f
Emmanuel <Wamuo>
2020-06-09 14:58:56 -0600

    Updates

---
+++ precache-manifest.3c856abaca0fdd7adf94f51484f619f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678b506cd9adba2a208c66b874bafe83",
+    "url": "/Lab-Home/index.html"
+  },
+  {
+    "revision": "2e31d401c3246c25a144",
+    "url": "/Lab-Home/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "37b7c9eccb8b0fa18f96",
+    "url": "/Lab-Home/static/js/2.c0bb849b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Lab-Home/static/js/2.c0bb849b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e31d401c3246c25a144",
+    "url": "/Lab-Home/static/js/main.2fa7d95c.chunk.js"
+  },
+  {
+    "revision": "3bdf2a241489f617ebcf",
+    "url": "/Lab-Home/static/js/runtime-main.0df61180.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Lab-Home/static/media/logo.5d5d9eef.svg"
+  }
+]);
